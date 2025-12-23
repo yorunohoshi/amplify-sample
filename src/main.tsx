5648f891117e8b30f8a71a8b2d44a4bb5b0775a0
Rikuo <xxrikuoxx1@gmail.com>
2025-12-24 00:06:22 +0900

    connect to backend

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Amplify } from "aws-amplify"
+import outputs from "../amplify_outputs.json"
+
+Amplify.configure(outputs)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
